f9e84dc9fdfe3612e1a540aeaccd8073c9c14029
Jaime García O Dowd <jgarciaodowd@victoria130.ad.danielcastelao.org>
2019-10-30 08:41:14 +0100

    inicio

---
+++ boletin6_1/Boletin6_1.java
@@ -0,0 +1,18 @@
+
+package boletin6_1;
+
+import java.util.Scanner;
+
+public class Boletin6_1 {
+
+public static void main(String[] args) {
+                       
+        Número obj1= new Número();
+        Scanner sc= new Scanner(System.in);
+        System.out.println(" num: ");
+        int num= sc.nextInt();
+        obj1.Número(num);
+                
+    }
+    
+}
---
+++ boletin6_1/Número.java
@@ -0,0 +1,31 @@
+
+package boletin6_1;
+
+public class Número {
+    
+    private int Número;
+    
+  public Número(){
+        
+    }
+    public Número(int num){
+        this.Número=num;
+    }
+        
+    public int getnum(Número num) {
+        return Número;
+    }
+
+    public int setnum(Número num) {
+       return Número;
+    }
+        public void Número(int num){
+        if(num>=0) {
+            System.out.println(" es positivo ");
+        } 
+        else {
+            System.out.println(" no es positivo ");
+        }
+    }
+     
+}
